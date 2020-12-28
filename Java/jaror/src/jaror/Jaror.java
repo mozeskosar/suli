dd8d62d4b306b370c07bba6be7430ba20322a047
Mecser Levente <56694893+mecserlevente@users.noreply.github.com>
2020-12-28 08:15:27 +0100

    Update Jaror.java

--- Java/jaror/src/jaror/Jaror.java
+++ Java/jaror/src/jaror/Jaror.java
@@ -1,52 +1,77 @@
-
-package jaror;
-
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.time.Duration;
 import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedHashSet;
 import java.util.Scanner;
 
 
-public class Jaror {
-
-    public static class jarmuvek{
-    
+public class jaror {
+    public static class adatok {
         LocalTime ido;
         String rendszam;
 
-        public jarmuvek(LocalTime ido, String rendszam) {
+        public adatok(LocalTime ido, String rendszam) {
             this.ido = ido;
             this.rendszam = rendszam;
         }
-    
-    
-    
     }
 
     public static void main(String[] args) throws FileNotFoundException {
-    
-       ArrayList<jarmuvek>lista=new ArrayList<jarmuvek>();
-        beolvas(lista);
-       int  munkaido=lista.get(lista.size()-1).ido.getHour()-lista.get(0).ido.getHour();
-        System.out.println("2. feladat: A járőrök "+munkaido+" órát dolgoztak!");
-      
-        
-    }
-    
-    public static void beolvas(ArrayList<jarmuvek>lista) throws FileNotFoundException{
-    
-        Scanner fileinput=new Scanner(new File("jarmu.txt"));
-        while(fileinput.hasNextLine()){
-        String data=fileinput.nextLine();
-        String tomb[]=data.split(" ");
-        lista.add(new jarmuvek(LocalTime.of(Integer.parseInt(tomb[0]), Integer.parseInt(tomb[1]), Integer.parseInt(tomb[2])),tomb[3]));
-        
+        ArrayList<adatok> lista = new ArrayList<>();
+        Scanner fileinput = new Scanner(new File("jarmu.txt"));
+        while (fileinput.hasNextLine()) {
+            String data = fileinput.nextLine();
+            String tomb[] = data.split(" ");
+            lista.add(new adatok(LocalTime.of(Integer.parseInt(tomb[0]), Integer.parseInt(tomb[1]), Integer.parseInt(tomb[2])), tomb[3]));
+
         }
         fileinput.close();
-    
+        int db = 0;
+
+        System.out.println("2. feladat");
+        System.out.println("Legalább " + (lista.get(lista.size() - 1).ido.getHour() - lista.get(0).ido.getHour() + 1) + " órát dolgoztak");
+
+        System.out.println("3.feladat");
+        System.out.println(lista.get(0).ido.getHour() + " óra: " + lista.get(0).rendszam);
+        for (int i = 2; i < lista.size(); i++) {
+            if (lista.get(i - 1).ido.getHour() != lista.get(i - 2).ido.getHour()) {
+                System.out.println(lista.get(i - 1).ido.getHour() + " óra: " + lista.get(i - 1).rendszam);
+            }
+        }
+        int bdb=0;
+        int kdb=0;
+        int mdb=0;
+        for(adatok e:lista){
+            if (e.rendszam.charAt(0)=='B'){
+                bdb++;
+            }
+            if (e.rendszam.charAt(0)=='K'){
+                kdb++;
+            }
+            if (e.rendszam.charAt(0)=='M'){
+                mdb++;
+            }
+        }
+        System.out.println("4. feladat");
+        System.out.println("B-s rendszámú autók: "+bdb+" db");
+        System.out.println("K-s rendszámú autók: "+kdb+" db");
+        System.out.println("M-s rendszámú autók: "+mdb+" db");
+
+        System.out.println("5. feladat");
+        int hely1=0;
+        int hely2=0;
+        int max=0;
+        long asd=0;
+        for (int i = 0; i < lista.size()-1; i++) {
+            if ((Duration.between(lista.get(i+1).ido,lista.get(i).ido).toMinutes())>max){
+                asd=(Duration.between(lista.get(i+1).ido,lista.get(i).ido).toMinutes());
+
+            }
+        }
+
+        System.out.println(lista.get(hely1).ido+" - "+lista.get(hely2).ido);
+        System.out.println(asd);
     }
-    
+
 }
